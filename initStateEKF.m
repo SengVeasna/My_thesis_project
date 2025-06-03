be1b8b3c85db8b0260ae4ff170a600c365acbd63
@Veasna_Seng <148682359+SengVeasna@users.noreply.github.com>
2025-06-03 16:24:14 +0700

    Delete initStateEKF.m

--- initStateEKF.m
+++
@@ -1,5 +0,0 @@
-function initState = initStateEKF(meanAccel, meanMag, meanAlti)
-    initState = zeros(6,1);
-    initState(1:4) = compact(ecompass(meanAccel, meanMag));
-    initState(5) = meanAlti;
-end
