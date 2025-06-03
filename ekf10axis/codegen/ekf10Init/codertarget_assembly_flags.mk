fb04747c672f4a274855141a9e6164b96988c8c2
@Veasna_Seng <148682359+SengVeasna@users.noreply.github.com>
2025-06-03 22:13:25 +0700

    Delete ekf10axis/codegen/ekf10Init directory

--- ekf10axis/codegen/ekf10Init/codertarget_assembly_flags.mk
+++
@@ -1,3 +0,0 @@
-ASFLAGS_ADDITIONAL = -mcpu=cortex-m4 -mthumb -mlittle-endian -mthumb-interwork -mfpu=fpv4-sp-d16 -mfloat-abi=hard
-STACK_SIZE = 200000
-TARGET_PKG_INSTALLDIR = C:/PROGRA~3/MATLAB/SUPPOR~1/R2024b/toolbox/target/SUPPOR~1/STM32F~1
