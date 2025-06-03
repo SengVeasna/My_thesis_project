ec3fba83b0dfd0f37320ea5e3e54c41743b8be73
@Veasna_Seng <148682359+SengVeasna@users.noreply.github.com>
2025-06-03 16:47:17 +0700

    Add files via upload

---
+++ ekf10axis/codegen/ekf10filter/rtGetNaN.c
@@ -0,0 +1,46 @@
+/*
+ * Company: UAVx Co., LTD
+ * Author1: HOM Ponlok
+ * Author2: SENG Veasna
+ * File: rtGetNaN.c
+ *
+ * Date  : 21-May-2025 16:12:11
+ */
+
+/*
+ * Abstract:
+ *       MATLAB for code generation function to initialize non-finite, NaN
+ */
+/* Include Files */
+#include "rtGetNaN.h"
+#include "rt_nonfinite.h"
+
+/*
+ * Function: rtGetNaN
+ * ======================================================================
+ *  Abstract:
+ * Initialize rtNaN needed by the generated code.
+ *  NaN is initialized as non-signaling. Assumes IEEE.
+ */
+real_T rtGetNaN(void)
+{
+  return rtNaN;
+}
+
+/*
+ * Function: rtGetNaNF
+ * =====================================================================
+ *  Abstract:
+ *  Initialize rtNaNF needed by the generated code.
+ *  NaN is initialized as non-signaling. Assumes IEEE
+ */
+real32_T rtGetNaNF(void)
+{
+  return rtNaNF;
+}
+
+/*
+ * File trailer for rtGetNaN.c
+ *
+ * [EOF]
+ */
---
+++ ekf10axis/codegen/ekf10filter/rtGetNaN.h
@@ -0,0 +1,31 @@
+/*
+ * Company: UAVx Co., LTD
+ * Author1: HOM Ponlok
+ * Author2: SENG Veasna
+ * File: rtGetNaN.h
+ *
+ * Date  : 21-May-2025 16:12:11
+ */
+
+#ifndef RTGETNAN_H
+#define RTGETNAN_H
+
+/* Include Files */
+#include "rtwtypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern real_T rtGetNaN(void);
+extern real32_T rtGetNaNF(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+/*
+ * File trailer for rtGetNaN.h
+ *
+ * [EOF]
+ */
